fee6ce84f7b90a9ccfd4082e90dd13b5c2678c77
alex-osi <osipchuk.alex@gmail.com>
2017-12-14 14:37:42 +0800

    8 commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     //popUp
-    $(".but_ml").magnificPopup();
+    // $(".but_ml").magnificPopup();
 
     //E-mail Ajax Send
     $("form").submit(function() { //Change
@@ -15,10 +15,20 @@ $(document).ready(function() {
             setTimeout(function() {
                 // Done Functions
                 th.trigger("reset");
-                $.magnificPopup.close();
+                // $.magnificPopup.close();
             }, 1000);
         });
         return false;
     });
 
 });
+
+// $('#exampleModal').on('show.bs.modal', function (event) {
+//     var button = $(event.relatedTarget) // Button that triggered the modal
+//     var recipient = button.data('') // Extract info from data-* attributes
+//     // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+//     // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+//     var modal = $(this)
+//     modal.find('.modal-title').text('New message to ')
+//     modal.find('.modal-body input').val(recipient)
+// })
