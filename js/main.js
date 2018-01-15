ff816de6072548afea916c1fb7ddcbcd7f261378
alex-osi <osipchuk.alex@gmail.com>
2018-01-15 19:41:46 +0800

    modal bootstrap

--- js/main.js
+++ js/main.js
@@ -23,6 +23,39 @@ $(document).ready(function() {
 
 });
 
+
+
+$('.sl').slick({
+    autoplay: true,
+    autoplaySpeed: 1500,
+});
+
+
+// $('.sl').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//         {
+//             breakpoint: 768,
+//             settings: {
+//                 arrows: false,
+//                 centerMode: true,
+//                 centerPadding: '40px',
+//                 slidesToShow: 3
+//             }
+//         },
+//         {
+//             breakpoint: 480,
+//             settings: {
+//                 arrows: false,
+//                 centerMode: true,
+//                 centerPadding: '40px',
+//                 slidesToShow: 1
+//             }
+//         }
+//     ]
+// });
 // $('#exampleModal').on('show.bs.modal', function (event) {
 //     var button = $(event.relatedTarget) // Button that triggered the modal
 //     var recipient = button.data('') // Extract info from data-* attributes
@@ -32,3 +65,36 @@ $(document).ready(function() {
 //     modal.find('.modal-title').text('New message to ')
 //     modal.find('.modal-body input').val(recipient)
 // })
+
+// Фильтрация блоков с помощью jQuery
+var fActive = '';
+
+function filterColor(color){
+    if(fActive != color){
+        $('#fil-1 div').filter('.'+color).slideDown();
+        $('#fil-1 div').filter(':not(.'+color+')').slideUp();
+        fActive = color;
+    }
+}
+
+$('.f-red').click(function(){ filterColor('red'); });
+$('.f-blue').click(function(){ filterColor('blue'); });
+$('.f-green').click(function(){ filterColor('green'); });
+
+$('.f-all').click(function(){
+    $('#fil-1 div').slideDown();
+    fActive = 'all';
+});
+
+//кнопка Вверх
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 100){
+        $('.top').addClass('active');
+        // alert(console.log(111))
+    } else {
+        $('.top').removeClass('active');
+    }
+});
+$('.top').click(function() {
+    $('html , body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+});
