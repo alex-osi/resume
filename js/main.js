16bd668c9611a8cabc95a763adb2a1fd3fc70ea9
alex-osi <osipchuk.alex@gmail.com>
2018-01-15 19:47:47 +0800

    button top

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-$(document).ready(function() {
-
-    //popUp
-    // $(".but_ml").magnificPopup();
-
-    //E-mail Ajax Send
-    $("form").submit(function() { //Change
-        var th = $(this);
-        $.ajax({
-            type: "POST",
-            url: "mail.php", //Change
-            data: th.serialize()
-        }).done(function() {
-            alert("Спасибо! Скоро я с Вами свяжусь");
-            setTimeout(function() {
-                // Done Functions
-                th.trigger("reset");
-                // $.magnificPopup.close();
-            }, 1000);
-        });
-        return false;
-    });
-
-});
-
-
-
-$('.sl').slick({
-    autoplay: true,
-    autoplaySpeed: 1500,
-});
+// $(document).ready(function() {
+//
+//     //popUp
+//     // $(".but_ml").magnificPopup();
+//
+//     //E-mail Ajax Send
+//     $("form").submit(function() { //Change
+//         var th = $(this);
+//         $.ajax({
+//             type: "POST",
+//             url: "mail.php", //Change
+//             data: th.serialize()
+//         }).done(function() {
+//             alert("Спасибо! Скоро я с Вами свяжусь");
+//             setTimeout(function() {
+//                 // Done Functions
+//                 th.trigger("reset");
+//                 // $.magnificPopup.close();
+//             }, 1000);
+//         });
+//         return false;
+//     });
+//
+// });
+//
+//
+//
+// $('.sl').slick({
+//     autoplay: true,
+//     autoplaySpeed: 1500,
+// });
 
 
 // $('.sl').slick({
